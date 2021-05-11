46f10c8dcae15b9108291156934c720fcf749038
Andrey Smirnov <muenchen.webas@gmail.com>
2021-05-11 17:44:08 +0200

    Продолжил разбивку основного шаблона на более мелкие части.

---
+++ resources/views/layouts/landingPage/team.blade.php
@@ -0,0 +1,14 @@
+<section class="wt-section">
+    <div class="container">
+        <div class="row justify-content-between align-items-center" data-aos="fade-left" data-aos-easing="linear" data-aos-delay="400">
+            <div class="col-md-7"> 
+                <h2 class="mb-4">Professional Team</h2>
+                <p class="text-muted">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius eligendi fugiat!</p>
+            </div>
+          
+		  <div class="col-md-5">
+               <img src="{{ asset('img/about/2.jpg') }}" width="90%" class="rounded-md" alt="">
+            </div>
+        </div>
+    </div>
+</section>
